8bf142d4c591129b260c1a61e6e877b5e53402bd
junlaoban <g_aojun01@163.com>
2021-12-09 17:56:46 +0800

    修改

--- src/network/cloudApi.js
+++ src/network/cloudApi.js
@@ -1,10 +1,14 @@
-
+/**
+ * @desc 获取版本日志
+ * @returns 版本记录
+ */
 export async function getVersionLog() {
   console.log(this, 'this', isLogin.call(this));
   if (!isLogin.call(this)) return
   return await this.$cloudbase.database().collection('version_log').get()
 }
 
+// 是否登录云开发 记录登录状态
 function isLogin() {
   const result = this.$cloudbase.auth().hasLoginState()
   if (!result) {
@@ -18,4 +22,9 @@ function isLogin() {
     return false
   }
   return true
+}
+
+// TODO 11
+export async function insertUser() {
+
 }
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,6 +1,7 @@
 import { CHANGE_LOGIN_STATE } from './mutations-type'
 export default {
   [CHANGE_LOGIN_STATE](state, payload) {
+    console.log('state', state)
     state.hasLoginState = payload
   }
 }
